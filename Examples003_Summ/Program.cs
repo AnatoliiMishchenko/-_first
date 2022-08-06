43694217039c1e518952f8b18486ca2395557b58
Anatolii Mishchenko <87mischenko@mail.ru>
2022-08-06 17:06:39 +0700

    Rondom summ

--- Examples003_Summ/Program.cs
+++ Examples003_Summ/Program.cs
@@ -1,4 +1,4 @@
-﻿int oneNumber = 5;
-int twoNumber = 3;
+﻿int oneNumber = new Random().Next(1,10);//Случаиное чило от 0 до 9
+int twoNumber = new Random().Next(1,10);
 int Summ = oneNumber + twoNumber;
 Console.Write("Сумма чисел " + oneNumber + " и " + twoNumber + " равна, " + Summ);

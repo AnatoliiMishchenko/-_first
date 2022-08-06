bdb83c67626bc4962f223362f4b01c6e18557a5a
Anatolii Mishchenko <87mischenko@mail.ru>
2022-08-06 17:30:50 +0700

    number max

---
+++ Examples006/Program.cs
@@ -0,0 +1,32 @@
+﻿int A = 1;
+int B =  2;
+int C = 9;
+int D = 3;
+int F = 4;
+int S = 6;
+int W = 5;
+int max = A;
+if(A > max) {
+    max = A;
+}
+if (B > max){
+    max = B;
+}
+if (C > max){
+    max = C;
+}
+if ( D > max){
+    max = D;
+}
+if ( F > max){
+    max = F;
+}
+if ( S > max){
+    max = S;
+}
+if ( W > max){
+    max = W;
+}
+
+
+Console.WriteLine("Максимальная гиря: " + max);

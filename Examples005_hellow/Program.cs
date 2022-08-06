bdf570d212eb220b0cf25d7523e810aa38ebc6e3
Anatolii Mishchenko <87mischenko@mail.ru>
2022-08-06 17:19:38 +0700

    Hellow nema

---
+++ Examples005_hellow/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите имя поьзлователя: ");
+string name = Console.ReadLine();
+if(name.ToLower() == "маша")
+{
+Console.WriteLine("Ура, это же Маша!!)))");
+}
+else
+{
+    Console.WriteLine("Привет " + name);
+}
